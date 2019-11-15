f42ef21c29ece7e8b9445bd59063f1d860a487f6
MarinaSamae <marina.samae.am01@gmail.com>
2019-11-16 03:04:45 +0700

    initain

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,19 +35,7 @@
 
 
 
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a class="genric-btn default circle arrow"  href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a class="genric-btn default circle arrow"  href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+         
             </div>
         		</div>
         	</div>
